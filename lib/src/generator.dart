d69236488d96ace430b1f4e666b734859eb0caba
Alexey Sharov <izolenta@users.noreply.github.com>
2018-10-21 21:06:47 +0300

    fix

--- lib/src/generator.dart
+++ lib/src/generator.dart
@@ -5,14 +5,15 @@ import 'package:buttons_field_gen/src/util/pseudo_random.dart';
 class FieldIndexGenerator {
   static int generate(int size, int minTurns) {
     PseudoRandom rand = new PseudoRandom();
+    int next = rand.nextInt(999999999);
     while (true) {
-      int next = rand.nextInt(999999999);
       BoardSolver solver = new BoardSolver();
       Board board = new Board(size, next);
       List<int> turns = solver.trySolveBoard(board, 5);
       if (turns.length <= minTurns) {
         return next;
       }
+      next++;
     }
   }
 }
